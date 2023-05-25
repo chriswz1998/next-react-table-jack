a9b2331dfbb331acad8ea6a94ef505eb5ae10de8
Scissorsfootdoctork <scissorwz@gmail.com>
2023-05-25 14:32:17 +0800

    submit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath: '/out',
+	// basePath: '/out',
 	async rewrites () {
 		return [
 			//接口请求 前缀带上/api-text/
-			{ source: '/api/:path*', destination: `http://10.18.56.54:9880/:path*` }
+			{ source: '/api/:path*', destination: `http://192.168.0.228:800/:path*` }
 		]
 	}
 }
